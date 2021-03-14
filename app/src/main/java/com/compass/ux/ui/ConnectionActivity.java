d7ed058a755166224198aaf4df655446cf4fe9f3
mgwang@luopan88.com <mgwang12345678>
2021-03-14 18:32:05 +0800

    屏蔽D-RTK监听1

--- app/src/main/java/com/compass/ux/ui/ConnectionActivity.java
+++ app/src/main/java/com/compass/ux/ui/ConnectionActivity.java
@@ -4633,14 +4633,14 @@ public class ConnectionActivity extends NettyActivity implements View.OnClickLis
                                 communication.setCode(200);
                                 communication.setResponseTime(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date()));
                                 NettyClient.getInstance().sendMessage(communication, null);
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        text_net_rtk_state.setText("RTK：" + rtkState.isRTKBeingUsed() + "");
-                                    }
-                                });
-                            }
 
+                            }
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    text_net_rtk_state.setText("RTK：" + rtkState.isRTKBeingUsed() + "");
+                                }
+                            });
                         }
                     });
                 }
