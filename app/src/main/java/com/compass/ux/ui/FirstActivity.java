61ec2e526a51685a42855ff70b28ae2f134998da
mgwang@luopan88.com <mgwang12345678>
2022-01-19 09:41:33 +0800

    解决4.15升级4.15.1后推流分辨率的问题

--- app/src/main/java/com/compass/ux/ui/FirstActivity.java
+++ app/src/main/java/com/compass/ux/ui/FirstActivity.java
@@ -87,7 +87,7 @@ public class FirstActivity extends AppCompatActivity {
 //                        Log.d("FileUtils", "FileUtils=" + mobileNum);
 //                    } else {
                         String filePath = FileUtils.createIfNotExist(Environment.getExternalStorageDirectory().getPath() + "/Shebei");
-                        FileUtils.writeString(filePath, "Mobile_04", "utf-8");
+                        FileUtils.writeString(filePath, "Mobile_01", "utf-8");
                         ApronApp.EQUIPMENT_ID = FileUtils.readString(file.getAbsolutePath(), "utf-8");
 //                    }
                     Intent intent = new Intent(FirstActivity.this, ConnectionActivity.class);
@@ -95,11 +95,7 @@ public class FirstActivity extends AppCompatActivity {
                     finish();
 //                }
 //            },2000);
-
-
         }
-
-
     }
 
 
